53d7998d85f6d83f07b27391bf1d059266427db3
ilhan76 <kudashov.ilya@inbox.ru>
2021-11-02 13:43:56 +0300

    В предыдущем коммите была ошибка. После того, как я вынес кастомные скоупы в отдельный пакет, приложение перестало собираться. Сейчас работает.

--- app/src/main/java/com/kudashov/testdagger/MainActivity.kt
+++ app/src/main/java/com/kudashov/testdagger/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
-import com.kudashov.testdagger.di.components.ActivityComponent
 import com.kudashov.testdagger.di.components.DaggerActivityComponent
 import javax.inject.Inject
 
@@ -13,21 +12,13 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var presenter: Presenter
 
-    private var activityComponent: ActivityComponent? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        activityComponent = DaggerActivityComponent.builder()
+        val activityComponent = DaggerActivityComponent.builder()
             .appComponent(appComponent)
             .build()
-
-        activityComponent!!.inject(this)
+        activityComponent.inject(this)
         presenter.doSmth()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        activityComponent = null
     }
 }
